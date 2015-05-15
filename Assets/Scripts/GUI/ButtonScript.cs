5bbb142d2e311c542480b1938437677c8a7ac157
Bejno <bejnofalk@gmail.com>
2015-05-15 22:50:57 +0200

    Added menu buttons functionality
    **MENUES! You can now start and quit the game**
    - Added start button functionality
    - Added quit button functionality
    - Added ButtonScript for basic button functions
    - Changed door in level_02 target scene to main_menu
    - Added main menu to build settings

---
+++ Assets/Scripts/GUI/ButtonScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+	public void LoadLevel(Object level) {
+		Application.LoadLevel (level.name);
+	}
+	public void LoadLevel(string level) {
+		Application.LoadLevel (level);
+	}
+
+	public void QuitGame() {
+		Application.Quit ();
+	}
+
+}
